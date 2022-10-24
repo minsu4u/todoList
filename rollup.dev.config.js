365d4132f60b544dc0cb8fdfe063938caf0d3c85
minsu gawk <myminsu@naver.com>
2022-10-24 19:32:17 +0900

    10-24 commit

---
+++ rollup.dev.config.js
@@ -0,0 +1,22 @@
+import serve from "rollup-plugin-serve";
+import rollupCommonConfig from "./rollup.common.config";
+import livereload from "rollup-plugin-livereload";
+
+const config = { ...rollupCommonConfig };
+
+config.watch = {
+  inclue: "src/**",
+};
+
+config.plugins = [
+  ...config.plugins,
+  serve({
+    host: "localhost",
+    port: 8080,
+    open: true,
+    contentBase: "dist",
+  }),
+  livereload("dist"),
+];
+
+export default config;
